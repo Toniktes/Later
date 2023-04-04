372ac000d82e0aac1f7e4e7cece68b5a1ab32ec6
Anton Teslia <teslyaanton@yandex.ru>
2023-04-04 23:07:57 +0300

    feat: add Entity User

--- src/main/java/ru/practicum/user/User.java
+++ src/main/java/ru/practicum/user/User.java
@@ -2,9 +2,24 @@ package ru.practicum.user;
 
 import lombok.Data;
 
-@Data
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+//@Data
+@Entity
+@Table(name = "users", schema = "public")
 public class User {
+    @Id
     private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
     private String email;
-    private String name;
+
 }
